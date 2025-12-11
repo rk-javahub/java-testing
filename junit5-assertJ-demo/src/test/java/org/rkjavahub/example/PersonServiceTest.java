3937351495771511ce2bed93fd33c6d7fe37ae55
Rohit Kumbhar <kumbharrohit13@gmail.com>
2025-12-11 16:26:45 +0530

    junit 5 changes

---
+++ junit5-assertJ-demo/src/test/java/org/rkjavahub/example/PersonServiceTest.java
@@ -0,0 +1,58 @@
+package org.rkjavahub.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonServiceTest {
+
+    PersonService personService;
+    public static Person person1;
+
+    @BeforeEach
+    void setUp() {
+        PersonRepository personRepository = new PersonRepository();
+        personService = new PersonService(personRepository);
+        person1 = new Person(17L,"Sachin","sachin12@gmail.com");
+        personService.create(person1);
+        System.out.println(person1.getName());
+    }
+
+    @Test
+    void create() {
+        Person person = new Person(7L,"Rohit","kumbharrohit13@gmail.com");
+        Person createdPerson = personService.create(person);
+        assertNotNull(createdPerson.getId());
+        assertEquals("Rohit", createdPerson.getName());
+        assertEquals("kumbharrohit13@gmail.com", createdPerson.getEmail());
+    }
+
+    @Test
+    void findById() {
+        person1 = new Person(17L,"Sachin","sachin12@gmail.com");
+        Person personOptional = personService.findById(17L).get();
+        //assertTrue(personOptional.isPresent());
+        assertEquals(17L, personOptional.getId());
+        assertEquals("Sachin", personOptional.getName());
+        assertEquals("sachin12@gmail.com", personOptional.getEmail());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
