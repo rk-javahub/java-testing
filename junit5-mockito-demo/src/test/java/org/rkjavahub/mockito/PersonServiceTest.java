08699eaf5d7bd18e2e85a7e3673754a6e75c7ce2
Rohit Kumbhar <kumbharrohit13@gmail.com>
2025-12-12 09:20:42 +0530

    Mockito test cases added

--- junit5-mockito-demo/src/test/java/org/rkjavahub/mockito/PersonServiceTest.java
+++ junit5-mockito-demo/src/test/java/org/rkjavahub/mockito/PersonServiceTest.java
@@ -1,30 +1,66 @@
 package org.rkjavahub.mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest {
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-    }
+    @Mock
+    PersonRepository personRepository;
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
+    @InjectMocks
+    PersonService personService;
 
-    @org.junit.jupiter.api.Test
+    @Test
     void login() {
+        //given
+        Person person = new Person(7L, "Rohit Kumbhar", "kumbharrohit13@gmail.com", "rohit123");
+        when(personRepository.findByEmailAndPassword(anyString(), anyString())).thenReturn(Optional.of(person));
+        //when
+        String token = personService.login("kumbharrohit13@gmail.com", "rohit123");
+        //then
+        assertThat(token).isNotNull();
+        verify(personRepository, times(1)).findByEmailAndPassword(anyString(), anyString());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
+    void loginFailure() {
+        //given
+        when(personRepository.findByEmailAndPassword(anyString(), anyString())).thenReturn(Optional.empty());
+        //when
+        String token = personService.login("kumbharrohit13@gmail.com", "wrongpassword");
+        //then
+        assertThat(token).isNull();
+        verify(personRepository, times(1)).findByEmailAndPassword(anyString(), anyString());
+    }
+
+    @Test
     void create() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void findByEmail() {
+        //given
+        Person person = new Person(7L, "Rohit Kumbhar", "kumbharrohit13@gmail.com", "rohit123");
+        when(personRepository.findByEmail(anyString())).thenReturn(Optional.of(person));
+        //when
+        Optional<Person> optionalPerson = personService.findByEmail("kumbharrohit13");
+        //then
+        assertThat(optionalPerson).isPresent();
+        assertThat(optionalPerson.get().getName()).isEqualTo("Rohit Kumbhar");
+        verify(personRepository, times(1)).findByEmail(anyString());
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void update() {
     }
 }
